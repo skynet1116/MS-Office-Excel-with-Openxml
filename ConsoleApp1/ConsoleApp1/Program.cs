f2c3bd83cf614059ca90e7dd9386c97024d8d534
junyao yu <yujunyao1116@gmail.com>
2017-08-10 15:39:54 +0800

    read

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        // The DOM approach.
+        // Note that the code below works only for cells that contain numeric values.
+        // The SAX approach.
+        static void ReadExcelFileSAX(string fileName)
+        {
+            using (SpreadsheetDocument spreadsheetDocument = SpreadsheetDocument.Open(fileName, false))
+            {
+                WorkbookPart workbookPart = spreadsheetDocument.WorkbookPart;
+                WorksheetPart worksheetPart = workbookPart.WorksheetParts.First();
+
+                OpenXmlReader reader = OpenXmlReader.Create(worksheetPart);
+                string text;
+                while (reader.Read())
+                {
+                    if (reader.ElementType == typeof(CellValue))
+                    {
+                        text = reader.GetText();
+                        Console.WriteLine(reader.ElementType + "\t" + text);
+                    }
+                    else if (reader.ElementType == typeof(SharedStringTablePart.share))
+                    {
+                        text = reader.GetText();
+                        Console.WriteLine(reader.ElementType + "\t" + text);
+                    }
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello word!");
+            String fileName = @"C:\Users\Administrator\Desktop\test.xlsx";
+            ReadExcelFileSAX(fileName);
+            SpreadsheetDocument output =
+                SpreadsheetDocument.Create(@"C:\Users\Administrator\Desktop\out.xlsx",
+                    SpreadsheetDocumentType.Workbook);
+
+
+            Console.ReadKey();
+        }
+    }
+}
