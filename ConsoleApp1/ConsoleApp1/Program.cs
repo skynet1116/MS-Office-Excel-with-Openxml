5ce943f56b032adb3f9871704b2db2d183ace35f
junyao yu <1575246955@qq.com>
2017-08-19 16:52:49 +0800

    实现表格复制

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,48 +1,72 @@
-﻿using System;
-using System.Linq;
-using DocumentFormat.OpenXml;
-using DocumentFormat.OpenXml.Packaging;
-using DocumentFormat.OpenXml.Spreadsheet;
+﻿
+using ClosedXML.Excel;
+using System;
 
-
-
-namespace ConsoleApp1
+namespace ClosedXML_Examples
 {
-    class Program
-    {
-        // The DOM approach.
-        // Note that the code below works only for cells that contain numeric values.
-        // The SAX approach.
-        static void ReadExcelFileSAX(string fileName)
-        {
-            using (SpreadsheetDocument spreadsheetDocument = SpreadsheetDocument.Open(fileName, false))
-            {
-                WorkbookPart workbookPart = spreadsheetDocument.WorkbookPart;
-                WorksheetPart worksheetPart = workbookPart.WorksheetParts.First();
 
-                OpenXmlReader reader = OpenXmlReader.Create(worksheetPart);
-                string text;
-                while (reader.Read())
-                {
-                    if (reader.ElementType == typeof(CellValue))
-                    {
-                        text = reader.GetText();
-                        Console.WriteLine(reader.ElementType + "\t" + text);
-                    }
-                }
-            }
-        }
+
+    public class Program
+    {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello word!");
-            String fileName = @"C:\Users\Administrator\Desktop\test.xlsx";
-            ReadExcelFileSAX(fileName);
-            SpreadsheetDocument output =
-                SpreadsheetDocument.Create(@"C:\Users\Administrator\Desktop\out.xlsx",
-                    SpreadsheetDocumentType.Workbook);
+            using (XLWorkbook wb = new XLWorkbook(@"C:\Users\Administrator\Desktop\固定资产编号明细(1).xlsx"))
+            {
+                IXLWorksheet ws = wb.Worksheets.Worksheet("资产明细"); //获得第一个Sheet。
+
+                //通过Worksheet(int position);函数获得sheet,position从1开始。
+
+                //不能使用ws.RowCount();获得很大的值。
+                //复制一份表格
+                var rngTable = ws.Range("G2:H7");       //表格切片
+                var excelTable = rngTable.CreateTable();//切片转化为表格
+                //行列坐标表示
+                int r = 2;
+                int c = 7;
+                //遍历所有可使用的行
+                var rows = ws.RowsUsed();
+                foreach (var row in rows)
+                {
+                    //检查是否是标题行
+                    //是的话跳过此行
+                    if (row.Cell(1).Value.ToString() == "公司名称")
+                    {
+                        Console.WriteLine(row.Cell(1).Value.ToString());
+                        continue;
+                    }
+                    r += 6;  //表格之间相隔6行
+                    ws.Cell(r, c).Value = excelTable;
+
+                    //保存更改
 
 
-            Console.ReadKey();
+                    //遍历所有的Cells
+
+                    foreach (var cell in row.Cells())
+
+                    {
+
+                        string tempst = cell.Value.ToString();
+                        Console.WriteLine(tempst);
+
+                    }
+
+                }
+                wb.SaveAs(@"C:\Users\Administrator\Desktop\固定资产编号明细(1).xlsx");
+                /*string filepath = @"C:\Users\Administrator\Desktop\固定资产输出.xlsx";
+                var workbook = new XLWorkbook();
+                var ws = workbook.Worksheets.Add("Sample Sheet");
+                // From a list of strings
+                var listOfStrings = new List<String>();
+                listOfStrings.Add("House");
+                listOfStrings.Add("Car");
+                ws.Cell(1, 1).Value = "Strings";
+                //ws.Cell(1, 1).AsRange().AddToNamed("Titles");
+                ws.Cell(2, 1).Value = listOfStrings;
+                workbook.SaveAs(filepath);*/
+            }
         }
+
+
     }
 }
