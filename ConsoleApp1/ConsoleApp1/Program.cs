9ff81f4542cfa9b59c64c05a816828a3d403ad14
junyao yu <yujunyao1116@gmail.com>
2017-08-21 15:03:32 +0800

    命令行交互界面&打包

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -12,22 +12,35 @@ namespace ClosedXML_Examples
     {
         static void Main(string[] args)
         {
-            string filepath = @"C:\Users\15752\Desktop\固定资产编号明细.xlsx";//输入文件目录
-            string savepath = @"C:\Users\15752\Desktop\输出.xlsx";//保存文件目录
-            //string filepath = @"C:\Users\Administrator\Desktop\固定资产编号明细(1).xlsx";
-            using (XLWorkbook wb = new XLWorkbook(filepath))
+            Console.WriteLine("欢迎使用 每项输入完毕后回车提交\n\r" +
+                              "请将目标文件放入程序同一目录中 \n\r" +
+                              "运行此程序前请确保目标文件和输出文件处于关闭状态\n\r" +
+                              "否则程序将出错且可能导致原数据丢失\n\r");
+            Console.WriteLine("请输入 目标文件 文件名");
+            Console.WriteLine("无需包含后缀名 目前仅支持xlsx");
+            string filepath = Console.ReadLine();//输入文件目录
+            string savepath = "输出.xlsx";//保存文件目录
+            Console.WriteLine("请输入 需要转换的sheet的名称");
+            string sheetname = Console.ReadLine();
+            Console.WriteLine("请输入 表格模版的坐标\n\r左上到右下 中间以英文冒号隔开  例：G2:H6");
+            string table = Console.ReadLine();
+            //行列坐标表示
+            int r, c;
+            Console.WriteLine("请输入 表格模版左上角的行坐标  数字表示");
+            r = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("请输入 表格模版左上角的列坐标  数字表示");
+            c = Convert.ToInt32(Console.ReadLine());
+            using (XLWorkbook wb = new XLWorkbook(filepath+".xlsx"))
             {
-                IXLWorksheet ws = wb.Worksheets.Worksheet("资产明细"); //获得第一个Sheet。
+                IXLWorksheet ws = wb.Worksheets.Worksheet(sheetname); //获得Sheet。
 
                 //通过Worksheet(int position);函数获得sheet,position从1开始。
 
                 //不能使用ws.RowCount();获得很大的值。
                 //复制一份表格
-                var rngTable = ws.Range("G2:H6");       //表格切片
+                var rngTable = ws.Range(table);       //表格切片
                 var excelTable = rngTable.CreateTable();//切片转化为表格
-                //行列坐标表示
-                int r = 2;
-                int c = 7;
+
                 //遍历所有可使用的行
                 var rows = ws.RowsUsed();
                 foreach (var row in rows)
@@ -54,13 +67,15 @@ namespace ClosedXML_Examples
                             break;
                         }
                         Console.WriteLine(tempst);
-                        ws.Cell(r, 8).Value = tempst;
+                        ws.Cell(r, c+1).Value = tempst;
                         r += 1;//写下一列数据
                     }
                     r += 1;//每个表之间空一行
                 }
                 wb.SaveAs(savepath);//保存结果
             }
+            Console.WriteLine("转换完毕 按任意键退出 输出文件初次打开可能需要excel的自动恢复");
+            Console.ReadKey();
         }
     }
 }
